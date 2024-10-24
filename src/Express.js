cc6fbc6bff639ac81ff361c7c6ac770108ac1458
Solarpaletten <solarpaletten@gmail.com>
2024-10-25 00:48:39 +0300

    cors

--- src/Express.js
+++
@@ -1,6 +0,0 @@
-const cors = require('cors');
-app.use(cors({
-  origin: ['https://my-react-app.onrender.com', 'http://localhost:3001'],
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true
-}));
