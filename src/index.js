fbc46205aecb6179a416f4f1b710a818a1f20259
Iryna Markovich <irina.markovich13@gmail.com>
2024-11-20 11:47:07 +0100

    fixed port

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
