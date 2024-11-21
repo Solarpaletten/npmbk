c32bd20a3d54900bed21f879728ce17f3658f774
Iryna Markovich <irina.markovich13@gmail.com>
2024-11-21 14:05:34 +0100

    fix column

--- src/controllers/saleController.js
+++ src/controllers/saleController.js
@@ -11,7 +11,7 @@ const getSales = async (req, res) => {
         warehouse.name AS warehouse, 
         buyer.name AS buyer,        
         sales.invoice_number,
-        sales.operation_type,
+        sales.invoice_type,
         sales.vat_rate,
         sales.total_amount,
         sales.currency,
